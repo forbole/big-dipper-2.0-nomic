93a7fbafa11390fb8c1a14b8a14c6ad2b73bdeed
yayay927 <isabelleya927@gmail.com>
2022-10-12 12:53:01 -0500

    finished fixing validator_detail folder

--- src/screens/validator_details/components/index.ts
+++ src/screens/validator_details/components/index.ts
@@ -1,13 +1,11 @@
 import Profile from './profile';
 import VotingPower from './voting_power';
-import Staking from './staking';
 import Blocks from './blocks';
 import ValidatorOverview from './validator_overview';
 
 export {
   Profile,
   VotingPower,
-  Staking,
   Blocks,
   ValidatorOverview,
 };
